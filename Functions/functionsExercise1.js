b46da4314978827f07bd8964b4d0c19211406f82
katarinjah <katarina.d.262@gmail.com>
2022-11-02 14:15:17 +0100

    Finished tasks 3-7 from Exercise _ Functions 1

---
+++ Functions/functionsExercise1.js
@@ -0,0 +1,130 @@
+// Write a program that calculates the maximum of two given numbers. 
+
+function maxNr (a,b) {
+
+    if (!isFinite(a) || !isFinite(b)) return "Invalid input";
+    if (a > b) return a;
+    if (b > a) return b;
+    return "Numbers are equal"
+}
+
+var x = maxNr(4,12);
+console.log(x)
+
+
+// Write a program that checks if a given number is odd.
+
+function oddNr(a) {
+
+    if (a % 2 === 1) return "The number is odd.";
+    if (a % 2 !== 1) return "The number is even.";
+}
+
+var f = oddNr(9);
+console.log(f)
+
+
+// Write a program that checks if a given number is a three digit long number.
+
+function threeDigitNr(z) {
+
+    var result = "";
+
+    if (z < 100 || z > 999) {
+        result = " does not have 3 digits";
+    } else if (z >= 100 || z <= 999) {
+        result = " has three digits";
+    }
+    return (z + result);
+}
+
+var x = threeDigitNr(567);
+console.log(x)
+
+
+// Write a program that calculates an arithmetic mean of four numbers.
+
+function arMean(a, b, c, d) {
+
+    var result = (a+b+c+d) / 4;
+    return result
+}
+
+var z = arMean(6,32,7,19);
+console.log(z)
+
+
+/* Write a program that draws a square of a given size. For example, if the size of
+square is 5 the program should draw:
+*****
+* *
+* *
+* *   
+*/
+
+function drawSquare(a, b) {
+    
+    var output = "";
+
+    for (var i = 0; i < a; i++) {
+    for (var j = 0; j < b; j++) {
+        if ( i === 0 || i === a - 1 || j === 0 || j === b - 1) {
+            output += "*";
+        } else {
+            output += " ";
+        }
+    }
+    if (i !== a - 1) output += "\n";    
+    }
+    return output
+}
+
+var x = drawSquare(4, 6);
+console.log(x)
+
+
+/* Write a program that draws a horizontal chart representing three given values.
+For example, if values are 5, 3, and 7, the program should draw:
+* * * * *
+* * *
+* * * * * * *
+*/
+
+function horChart (a, b, c) {
+    
+    var output = "";
+
+    for (var i = 0; i < a; i++) {
+        output += "*"; }   
+    if (i !== a - 1) output += "\n";  
+
+    for (var j = 0; j < b; j++) {
+        output += "*"; }
+    if (j !== b - 1) output += "\n";
+
+    for (var k = 0; k < c; k++) {
+        output += "*";
+    }
+            return output
+        }
+
+var g = horChart(4, 11, 7);
+console.log(g)
+
+
+// Write a program that calculates a number of digits of a given number.
+
+function numOfDigits(a) {
+    var result = 0;
+
+    if (a >= 1) ++result;
+  
+    while (a / 10 >= 1) {
+      a /= 10;
+      ++result;
+    }
+    return result;
+  }
+
+  var f = numOfDigits(6398657);
+  console.log(f)
