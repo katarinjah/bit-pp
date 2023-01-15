97af904b78df6da70a2a579d746e90b8943eeeb2
katarinjah <katarina.d.262@gmail.com>
2023-01-15 17:45:34 +0100

    Inheritance homework

--- Objects/inheritance.js
+++ Objects/inheritance.js
@@ -1,41 +1,43 @@
 "use strict";
 
-// Task 1: Employees and Managers:
+// Task 1: Employees and Managers
 
-function Person(name, surname) {
-    this.name = name;
-    this.surname = surname;
-};
+class Person {
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 
-Person.prototype.getFullName = function() {
-    console.log(this.name + " " + this.surname)
-};
+    getFullName = function() {
+        console.log(this.name + " " + this.surname);
+    }
+}
 
 var person = new Person("Zika", "Zikic");
 //console.log(person);
 //person.getFullName();
 
-function Employee(name, surname, job, salary) {
-    Person.call(this, name, surname);
-    this.job = job;
-    this.salary = salary; 
-};
 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+class Employee extends Person {
+    constructor(name, surname, job, salary) {
+        super(name, surname);
+        this.job = job;
+        this.salary = salary; 
+    }
 
-Employee.prototype.getData = function() {
-    console.log(this.name + " " + this.surname + ", " + this.salary)
-};
+    getData = function() {
+        console.log(this.name + " " + this.surname + ", " + this.salary)
+    }
 
-Employee.prototype.getSalary = function() {
-    console.log(this.salary);
-};
+    getSalary = function() {
+        console.log(this.salary);
+    }
 
-Employee.prototype.increaseSalary = function() {
-    this.salary = this.salary * 1.1;
-    console.log(this.salary);
-};
+    increaseSalary = function() {
+        this.salary = this.salary * 1.1;
+        console.log(this.salary);
+    }
+}
 
 var employee = new Employee("Pera", "Peric", "Programmer", 2500);
 //console.log(employee);
@@ -44,69 +46,70 @@ var employee = new Employee("Pera", "Peric", "Programmer", 2500);
 //employee.increaseSalary();
 
 
-function Developer(name, surname, job, salary, specialization) {
-    Employee.call(this, name, surname, job, salary);
-    this.specialization = specialization;
-};
+class Developer extends Employee {
+    constructor(name, surname, job, salary, specialization) {
+        super(name, surname, job, salary);
+        this.specialization = specialization;
+    }
 
-Developer.prototype = Object.create(Employee.prototype);
-Developer.prototype.constructor = Developer;
+    getSpecialization = function() {
+        console.log(this.specialization)
+    }
+}
 
-Developer.prototype.getSpecialization = function() {
-    console.log(this.specialization)
-};
-
-var developer = new Developer("Laza", "Lazic", "Programmer", 2200, "Frontend");
+var developer = new Developer("Laza", "Lazic", "Programmer", 2600, "Frontend");
 //console.log(developer);
 //developer.getSpecialization();
 
-function Manager(name, surname, job, salary, specialization, department) {
-    Developer.call(this, name, surname, job, salary, specialization);
-    this.department = department;
-};
 
-Manager.prototype = Object.create(Developer.prototype);
-Manager.prototype.constructor = Manager;
+class Manager extends Developer {
+    constructor(name, surname, job, salary, specialization, department) {
+        super(name, surname, job, salary, specialization);
+        this.department = department;
+    }
 
-Manager.prototype.getDepartment = function() {
-    console.log(this.department)
-};
+    getDepartment = function() {
+        console.log(this.department)
+    }
 
-Manager.prototype.changeDepartment = function(newDept) {
-    this.department = newDept;
-};
+    changeDepartment = function(newDept) {
+        this.department = newDept;
+        console.log(this.department);
+    }
+}
 
-var manager = new Manager("Pera", "Peric", "Programmer", 10000, "WebDev", "Backend");
+var manager = new Manager("Pera", "Peric", "Programmer", 3000, "Backend", "WebDev");
 //console.log(manager);
 //manager.getDepartment();
 //manager.changeDepartment("Frontend");
-//console.log(manager);
 
 
-// Task 2: Applications:
 
-function App(name, licence, stars) {
-    this.name = name;
-    this.licence = licence;
-    this.stars = stars;
-};
+// Task 2: Applications
 
-App.prototype.isCCLicence = function() {
-    if(this.licence === "CC" || "Creative Commons") {
-        return true;
-    } else {
-        return false;
-    };
-};
+class App {
+    constructor(name, licence, stars) {
+        this.name = name;
+        this.licence = licence;
+        this.stars = stars;
+    }
 
-App.prototype.like = function() {
-    this.stars += 1;
-    return this.stars;
-};
+    isCCLicence = function() {
+        if(this.licence === "CC" || this.licence === "Creative Commons") {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-App.prototype.showStars = function() {
-    console.log(this.stars + " star rated");
-};
+    like = function() {
+        return(this.stars += 1);
+    }
+
+    showStars = function() {
+        console.log(this.stars + " star rated");
+    }
+}
 
 var app = new App("SuperApp", "CC", 3);
 //console.log(app);
@@ -115,58 +118,59 @@ var app = new App("SuperApp", "CC", 3);
 //app.showStars();
 
 
-function WebApp(name, url, technologies, licence, stars) {
-    App.call(this, name, licence, stars);
-    this.url = url;
-    this.technologies = technologies;
-};
+class WebApp extends App {
+    constructor(name, url, technologies, licence, stars) {
+        super(name, licence, stars);
+        this.url = url;
+        this.technologies = technologies;
+    }
 
-WebApp.prototype = Object.create(App.prototype);
-WebApp.prototype.constructor = WebApp;
+    getData = function() {
+        console.log(this.name + "\n" + this.url + "\n" + this.technologies + "\n" + this.licence + "\n" + this.stars)
+    }
 
-WebApp.prototype.getData = function() {
-    console.log(this.name + "\n" + this.url + "\n" + this.technologies + "\n" + this.licence + "\n" + this.stars)
-};
-
-WebApp.prototype.reactBased = function() {
-    if (this.technologies.includes("React")) {
-        return true;
-    } else {
-        return false;
-    };
-};
+    reactBased = function() {
+        if (this.technologies.includes("React")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 
 var webApp = new WebApp("AwesomeApp", "https://blahblah.com", ["Angular", "React", "Vue"], "BB", 4);
 //console.log(webApp);
 //webApp.getData();
 //console.log(webApp.reactBased());
+//console.log(webApp.isCCLicence());
 //console.log(webApp.like());
+//webApp.showStars();
 
 
-function MobileApp(name, platforms, licence, stars) {
-    App.call(this, name, licence, stars);
-    this.platforms = platforms;
-};
+class MobileApp extends App {
+    constructor(name, platforms, licence, stars) {
+        super(name, licence, stars);
+        this.platforms = platforms;
+    }
 
-MobileApp.prototype = Object.create(App.prototype);
-MobileApp.prototype.constructor = MobileApp;
+    getData = function() {
+        console.log(this.name + "\n" + this.platforms + "\n" + this.licence + "\n" + this.stars)
+    }
 
-MobileApp.prototype.getData = function() {
-    console.log(this.name + "\n" + this.platforms + "\n" + this.licence + "\n" + this.stars)
-};
+    forAndroid = function() {
+        if (this.platforms.includes("Android")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 
-MobileApp.prototype.forAndroid = function() {
-    if (this.platforms.includes("Android")) {
-        return true;
-    } else {
-        return false;
-    };
-};
-
-var mobileApp = new MobileApp("GreatApp", ["iOS", "Android"], "cclicence", 4);
+var mobileApp = new MobileApp("GreatApp", ["iOS", "Android"], "Creative Commons", 4);
 //console.log(mobileApp);
 //mobileApp.getData();
 //console.log(mobileApp.forAndroid());
+//console.log(mobileApp.isCCLicence());
 //console.log(mobileApp.like());
 //mobileApp.showStars();
 
