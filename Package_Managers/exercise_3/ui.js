c4c460069155298f87c6ffd353a50a4acaa777fb
katarinjah <katarina.d.262@gmail.com>
2023-02-28 22:11:31 +0100

    Added styling

--- Package_Managers/exercise_3/ui.js
+++ Package_Managers/exercise_3/ui.js
@@ -3,7 +3,7 @@ export const renderPosts = (posts) => {
         posts.forEach((post) => {
           html += `
             <div class="post" id="${post.id}">
-                <h5 class="title">${post.title}</h5>
+                <h4 class="title">${post.title}</h5>
                 <p class="intro">${post.intro}</p>
             </div>
             `;
